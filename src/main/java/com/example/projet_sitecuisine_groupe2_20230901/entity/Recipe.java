40f6ece28c6ce640243038ccc12a8f3e9d60159b
Steeve Lowenski <steeve.lowenski@gmail.com>
2023-09-01 13:04:57 +0200

    class recipe

---
+++ src/main/java/com/example/projet_sitecuisine_groupe2_20230901/entity/Recipe.java
@@ -0,0 +1,78 @@
+package com.example.projet_sitecuisine_groupe2_20230901.entity;
+
+import javax.persistence.*;
+
+@Entity// Table pour hibernate
+@Table(name="recipe")// Nom de la table dans la bdd
+@SequenceGenerator(name="recipe_gen",sequenceName="recipe_seq",initialValue=1,allocationSize=1)
+public class Recipe {
+
+    @Id// Clé primaire
+    @GeneratedValue(generator = "game_gen")
+    private Integer id;
+
+    @Column(name = "category") // Nom de la colonne dans la table
+    private String category;
+
+    @Column(name = "name")// Nom de la colonne dans la table
+    private String name;
+
+    @Column(name = "recipe_method")// Nom de la colonne dans la table
+    private String recipe_method;
+
+    @Column(name = "author") // Nom de la colonne dans la table
+    private String author;
+
+    public Recipe() {}
+
+    public Recipe(String category, String name, String recipe_method, String author) {
+        this.category = category;
+        this.name = name;
+        this.recipe_method = recipe_method;
+        this.author = author;
+    }
+
+    public Recipe(Integer id, String category, String name, String recipe_method, String author) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+        this.recipe_method = recipe_method;
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRecipe_method() {
+        return recipe_method;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRecipe_method(String recipe_method) {
+        this.recipe_method = recipe_method;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
